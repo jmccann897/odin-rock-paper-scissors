37b8fe42ab46da57232e70b6457a2e6f9569ab00
Josh <55717114+jmccann897@users.noreply.github.com>
2026-04-25 23:50:03 +0100

    rps ui updates

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Odin Rock Paper Scissors repository for the Foundations Course: Rock Paper Sciss
 
 ## Description
 
-A simple Rock Paper Scissors JavaScript console game.
+A simple Rock Paper Scissors JavaScript game updated from being played in the console to clicking UI buttons.
 
-The user is prompted to enter **rock**, **paper**, or **scissors** to play a round against the computer. The computer randomly selects its move each round.
+The user can click the **rock**, **paper**, or **scissors**  buttons to play a round against the computer. The computer randomly selects its move each round.
 
 After each round:
-- The player's choice and the computer's choice are displayed in the browser console in developer tools.
+- The player's choice and the computer's choice are displayed below the options.
 - The result of the round (win, lose, or draw) is shown.
 - The running score for the player, computer, and draws is updated.
 
-The game continues in a loop until the user enters '**x**' to exit.
+The game continues until either the user or computer reaches a score of 5. The user can click '**x**' to exit at any time.
--- javascript.js
+++ javascript.js
@@ -1,34 +1,5 @@
-// ***Console game for rock paper scissors***
-
-/*  **Requirements**
-Ask user if they want to play rock paper scissors
-Loop the game logic unless user inputs an exit command?
-Take input from user - string of rock/paper/scissors
-Create a random turn representing the computer
-Compare the two values to determine winner
-Return results to player
-Ask if they wish to continue
-*/ 
-
-/* **Code plan**
-
-Input - string - if at any time, use x to end game
-
-Game - input rock, paper, scissors (ignore case)
-
-Use random to pick value 0-2 to determine computer selection of rock paper scissors
-
-winning, draw and lose logic
-Rock > scissors
-Paper > rock
-Scissors > paper
-
-Return result to user
-Ask if they wish to continue
-
-Bonus - track results and update on results
-*/
-
+// In our UI, the player should be able to play the game by clicking 
+// on buttons rather than typing their answer in a prompt.
 
 // Global variables
 let drawMessage = "It's a draw!";
@@ -42,38 +13,36 @@ let drawScore = 0;
 
 const computerOptions = ["rock", "paper", "scissors"];
 
-let numberOfRounds = 5;
+// DOM refs
+const resultsDiv = document.getElementById("results");
+const scoreDiv = document.getElementById("score");
+const gameOverDiv = document.getElementById("gameOver");
 
-// main
-playGame(numberOfRounds);
+// Listeners on buttons
+// Switch on each id - its humanChoice
+document.getElementById('options').addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
 
-// Helper functions
-function playGame(rounds){
-    let playARound = true;
-    let roundCount = 0
-
-    while(playARound && roundCount < rounds){
-
-        let humanChoice = getHumanChoice();
-        console.log("You chose:", humanChoice);
-
-        if(humanChoice.toLowerCase() === "x"){
-            console.log("Thanks for playing!");
-            break;
+        // check for x click
+        const choice = e.target.id;
+        if(choice === "end"){
+            alert(`Game ended!
+                Final Score: 
+                You: ${humanScore}
+                Computer: ${computerScore}
+                Draws: ${drawScore}`);
+            
+            return;
         }
 
-        let compChoice = getComputerChoice();
+        // Stop if someone reaches 5
+        if (winCheck()) return;
 
-        if (!computerOptions.includes(humanChoice)) {
-            console.log("Invalid choice. Please enter rock, paper, scissors, or x to exit.");
-            continue;
-        }
-
-        playRound(humanChoice, compChoice);
-        roundCount++;
-        showScore(scores);
+        playRound(e.target.id);
+        showScore();
+        winCheck();
     }
-}
+});
 
 function getRandomInt(maxNum){
     return Math.floor(Math.random() * maxNum);
@@ -82,57 +51,51 @@ function getRandomInt(maxNum){
 function getComputerChoice(){
     let randInt = getRandomInt(3);
     let computerChoice = computerOptions[randInt];
-
-    //console.log("random int:", randInt);
-    console.log("Computer chose:", computerChoice);
-
+    resultsDiv.textContent = `Computer chose: ${ computerChoice}`;
     return computerChoice;
 }
 
-function getHumanChoice(){
-    return prompt("Enter rock, paper, scissors (or x to exit)").trim().toLowerCase();
-}
+function playRound(humanChoice){
 
-
-function playRound(humanChoice, compChoice ){
+    let compChoice = getComputerChoice();
     // game logic
     switch (humanChoice)
     {
         case "rock":
             if (compChoice === "rock"){
-                console.log(drawMessage);
+                resultsDiv.textContent = drawMessage;
                 drawScore++;
             } else if (compChoice === "scissors"){
-                console.log(winMessage);
+                resultsDiv.textContent = winMessage;
                 humanScore++;
             } else {
-                console.log(loseMessage);
+                resultsDiv.textContent = loseMessage;
                 computerScore++;
             }
             break;
 
         case "paper":
             if (compChoice === "paper"){
-                console.log(drawMessage);
+                resultsDiv.textContent = drawMessage;
                 drawScore++;
             } else if (compChoice === "rock"){
-                console.log(winMessage);
+                resultsDiv.textContent = winMessage;
                 humanScore++;
             } else {
-                console.log(loseMessage);
+                resultsDiv.textContent = loseMessage;
                 computerScore++;
             }
             break;
 
         case "scissors":
             if (compChoice === "scissors"){
-                console.log(drawMessage);
+                resultsDiv.textContent = drawMessage;
                 drawScore++;
             } else if (compChoice === "paper"){
-                console.log(winMessage);
+                resultsDiv.textContent = winMessage;
                 humanScore++;
             } else {
-                console.log(loseMessage);
+                resultsDiv.textContent = loseMessage;
                 computerScore++;
             }
             break;
@@ -143,8 +106,26 @@ function playRound(humanChoice, compChoice ){
 
 function showScore(){
     // Show score
-    console.log("Score:");
-    console.log("Your Score:", humanScore);
-    console.log("Computer:", computerScore);
-    console.log("Draws:", drawScore);
+    let scoreContent = `Your Score: ${humanScore}
+    Computer: ${computerScore}
+    Draws ${drawScore}`;
+    scoreDiv.textContent = scoreContent;
+    
+}
+
+function winCheck(){
+    let gameOverMessage = "";
+    if(humanScore == 5){
+        gameOverDiv.textContent = "You win the game!";
+        alert("You win the game!");
+        return true;
+    }
+
+    if(computerScore == 5){
+        gameOverDiv.textContent = "Computer wins the game!";
+        alert("Computer wins the game!");
+        return true;
+    }
+
+    return false;
 }
